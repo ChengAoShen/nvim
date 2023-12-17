8fda3d922bc2272f5259d741aa9659cc57cf55f5
shenvinci <shenvinci@gmail.com>
2023-12-17 23:11:44 +0800

    update init

--- init.lua
+++ init.lua
@@ -1,12 +1,3 @@
 require("config.basic")
 require("config.keymaps")
 require("lazy").setup("plugins")
-require("lazy").setup({
-    {
-        'kkoomen/vim-doge',
-        config = function()
-            vim.g.doge_doc_standard_python = 'google'
-        end,
-    },
-})
-require("languages.python")
