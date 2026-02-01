e89c84bc56b5ba13fe2ae86b774a03b1a5ffd2e2
ChengAo Shen <chengaoshen2002@gmail.com>
2026-02-01 15:18:02 -0600

    use formate

---
+++ lua/plugins/format.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    javascript = { "prettierd" },
+                    javascriptreact = { "prettierd" },
+                    typescript = { "prettierd" },
+                    typescriptreact = { "prettierd" },
+                    json = { "prettierd" },
+                    jsonc = { "prettierd" },
+                    yaml = { "prettierd" },
+                    markdown = { "prettierd" },
+                    html = { "prettierd" },
+                    css = { "prettierd" },
+                    scss = { "prettierd" },
+                    less = { "prettierd" },
+                    python = { "ruff_format" },
+                },
+                format_on_save = function()
+                    return {
+                        lsp_fallback = true,
+                        async = false,
+                        timeout_ms = 2000,
+                    }
+                end,
+            })
+        end,
+    },
+}
